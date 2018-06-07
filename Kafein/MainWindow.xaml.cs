7629dc17be66ff161856492bfdf8d9f95dc8ae2e
QuanDao <daonguyenquan@hotmail.com>
2018-06-07 11:39:27 +0700

    hamburger

--- Kafein/MainWindow.xaml.cs
+++ Kafein/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Kafein
 {
@@ -24,6 +25,11 @@ namespace Kafein
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void HamburgerMenuControl_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
+        {
+            HamburgerMenuControl.Content = e.InvokedItem;
         }
     }
 }
