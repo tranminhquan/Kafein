061953eaec5613c307660aaa6a1908dc766775c2
QuanDao <daonguyenquan@hotmail.com>
2018-06-07 17:43:42 +0700

    remove sign in

---
+++ Kafein/Model/Interface/IHavePassword.cs
@@ -0,0 +1,10 @@
+﻿using System.Security;
+
+namespace Kafein.Model.Interface
+{
+    public interface IHavePassword
+    {
+        SecureString Password { get; }
+        SecureString ConfirmPassword { get; }
+    }
+}
