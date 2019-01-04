536ec5e65e6902462422cffba883e92cc7a16bbd
Kiet Vo <geforce2409@gmail.com>
2019-01-04 22:36:36 +0700

    Add setup NUnit (Unit test)

---
+++ KafeinTest/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KafeinTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            //Assert.AreEqual();
+        }
+        [Test]
+        public void TestCase2()
+        {
+            //Assert.AreEqual();
+        }
+    }
+}
